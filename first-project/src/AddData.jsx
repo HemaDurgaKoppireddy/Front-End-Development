8e67a9471510d86e62e55248563a247451418081
HemaDurgaKoppireddy <23P31A0530@acet.ac.in>
2025-10-10 21:00:48 +0530

    React Task

---
+++ first-project/src/AddData.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useState, useEffect, useContext } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./AddData.css"
+
+function AddData() {
+    return(
+        <>
+        <div></div>
+        </>
+    )
+}
+
+export default AddData;
---
+++ first-project/src/ViewData.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useState, useEffect, useContext } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./ViewData.css"
+
+function ViewData() {
+
+}
+
+export default ViewData;
